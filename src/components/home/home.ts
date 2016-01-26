bbd44f4f0b75ae8098931ed3c9e857352c8e15ff
Reid Blomquist <reid@theprogrampdx.com>
2016-01-26 12:03:13 -0800

    pulling out some cruft, working on getting a handle on ng2 forms

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from 'angular2/core';
-import {Http, Response} from 'angular2/http'
+import {Component} from 'angular2/core';
+import {NgForm} from 'angular2/common';
+import {Http, Response, Headers} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject';
 
 @Component({
   selector: 'home',
@@ -7,9 +10,30 @@ import {Http, Response} from 'angular2/http'
     <h2>Home</h2>
     <h3>
       <div *ngFor="#country of allCountries.countries">
-        {{country}}
+        {{country.Name}}
+        {{country.Code}}
       </div>
     </h3>
+    <h4>Add yourself a damn country, son</h4>
+    <form #f="form" (ng-submit)="postCountry(f.value)">
+      <div ng-control-group="countryForm">
+          <label for="code">Code</label>
+          <input
+            type="text"
+            id="code"
+            ng-control="code"
+            required>
+
+          <label for="name">Name</label>
+          <input
+            type="name"
+            id="name"
+            ng-control="name"
+            required>
+      </div>
+
+      <button>Add</button>
+    </form>
   `
 })
 
@@ -18,6 +42,7 @@ export class Home {
   result: Object;
   http: Http;
   allCountries: any;
+  postCountryResponse = new Country();
 
   constructor(http: Http) {
     this.http = http;
@@ -26,9 +51,31 @@ export class Home {
 
   loadCountries(){
     this.allCountries = {};
-    this.http.get('http://localhost:8080/countries').toPromise()
+    this.http.get(Api.BASE_URL + '/countries').toPromise()
       .then((res: Response) => {
         this.allCountries.countries = res.json();
       });
   }
+
+  postCountry(){
+
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    this.http.post(Api.BASE_URL + '/countries', JSON.stringify({
+          Code: 'US',
+          Name: 'United States'
+        }), {headers: headers})
+        .map((res:Response) => res.json())
+        .subscribe((res:Country) => this.postCountryResponse = res);
+  }
+}
+
+class Country{
+  Code:string;
+  Name:string;
+}
+
+export class Api{
+  public static get BASE_URL():string {return "http://localhost:8080"};
 }
